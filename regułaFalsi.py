a8cd355b46a7deb0959fb26ca8af7ab2f63bf2e7
Karol66 <104461694+Karol66@users.noreply.github.com>
2023-04-17 11:49:51 +0200

    Add files via upload

--- regułaFalsi.py
+++ regułaFalsi.py
@@ -1,108 +1,91 @@
-import sympy as sp
+import math
 
+def licz_pochodne(f, x, h=1e-6):
+    """
+    Funkcja oblicza numerycznie wartości pierwszej i drugiej pochodnej funkcji f(x) w punkcie x.
+
+    Parametry:
+    f: funkcja, której pochodne mają zostać obliczone
+    x: punkt, w którym pochodne są obliczane
+    h: wartość kroku różniczkowania (domyślnie 1e-6)
+
+    Zwraca:
+    df1: wartość pierwszej pochodnej funkcji f(x) w punkcie x
+    df2: wartość drugiej pochodnej funkcji f(x) w punkcie x
+    """
+    fx = f(x)
+    fxh = f(x + h)
+    fx2h = f(x + 2 * h)
+    df1 = (fxh - fx) / h
+    df2 = (fx2h - 2 * fxh + fx) / (h ** 2)
+    return df1, df2
 
 # Definiujemy funkcję
 def f(x):
-    return x ** 3 - 2 * x ** 2 - 4 * x - 7
+    return x**2
 
-# Twierdzenie Bolzano-Cauchy'ego
-def twBolzanoCauchyego(a, b):
-    # Czy f(a)*f(b) < 0 ?
-    twierdzenie = f(a) * f(b)
-    if (twierdzenie < 0):
-        return True
-    else:
-        return False
+# Obliczamy pochodne w punkcie x=
+x = float(input("Podaj punkt, w którym chcesz obliczyć pochodne funkcji: "))
+df1, df2 = licz_pochodne(f, x)
 
+# Wyświetlamy wyniki
+print(f"Wartość pierwszej pochodnej funkcji sin(x) w punkcie x=1 wynosi: {df1}")
+print(f"Wartość drugiej pochodnej funkcji sin(x) w punkcie x=1 wynosi: {df2}")
 
-# Liczymy pochodną I stopnia:
-def pochodne():
-    # Definiujemy zmienną symboliczną
-    x = sp.Symbol('x')
+def metoda_regula_falsi(f, a, b, epsilon=1e-6):
+    """
+    Metoda Reguły Falsi służy do znajdowania przybliżonego miejsca zerowego funkcji f(x) na przedziale [a, b].
 
-    # Obliczamy pochodną funkcji f
-    df = sp.diff(f(x), x)
+    Parametry:
+    f: funkcja, dla której szukamy miejsca zerowego
+    a, b: końce przedziału, na którym poszukujemy miejsca zerowego
+    epsilon: wartość błędu (domyślnie 1e-6)
 
-    # Zwracamy wynik
-    return df
+    Zwraca:
+    x2: przybliżone miejsce zerowe funkcji f(x) na przedziale [a, b]
+    """
 
+    # Sprawdzamy warunek konieczny istnienia miejsca zerowego
+    if f(a) * f(b) > 0:
+        raise ValueError("Funkcja nie spełnia warunków istnienia miejsca zerowego na przedziale [a, b].")
 
-# Liczymy pochodną II stopnia:
-def pochodne2():
-    # Definiujemy zmienną symboliczną
-    x = sp.Symbol('x')
+    # Inicjalizujemy zmienne
+    fa = f(a)
+    fb = f(b)
+    x2 = a
+    df2 = 0
 
-    # Obliczamy pochodną funkcji f
-    df = sp.diff(pochodne(), x)
+    while abs(fb) > epsilon:
 
-    # Zwracamy wynik
-    return df
+        # Obliczamy wartości pochodnych funkcji w punkcie x2
+        df1, df2 = licz_pochodne(f, x2)
 
-# Sprawdzamy czy:
-# f'(x0) * f''(x0) > 0
-def sprawdzenie(a, b):
-    x = sp.Symbol('x')
+        # Obliczamy wartość nowego punktu
+        x3 = b - fb * (b - a) / (fb - fa - df2 * (b - a))
 
-    f_valueA = f(a)
-    df_valueA = pochodne().subs(x, a)
-    df_valueA2 = pochodne2().subs(x, a)
+        # Sprawdzamy warunek stopu
+        if abs(x3 - x2) < epsilon:
+            break
 
-    f_valueB = f(b)
-    df_valueB = pochodne().subs(x, b)
-    df_valueB2 = pochodne2().subs(x, b)
+        # Przesuwamy punkty
+        a = b
+        fa = fb
+        b = x3
+        fb = f(b)
+        x2 = x3
 
-    # print(f_valueA)
-    # print(df_valueA)
-    # print(df_valueA2)
-    #
-    # print(f_valueB)
-    # print(df_valueB)
-    # print(df_valueB2)
+    return x2
 
-    # Czy: f'(a) * f''(a) > 0  i Czy: f(a) * f''(a) > 0
-    if (df_valueA * df_valueA2 > 0 or df_valueB * df_valueB2 > 0):
-        return True
-    else:
-        return False
+def f(x):
+    return x ** 3 - 3 * x + 1
 
+# Ustawiamy wartości początkowe
+a = -1
+b = 1
+epsilon = 1e-6
 
+# Wywołujemy funkcję
+x0 = metoda_regula_falsi(f, a, b, epsilon)
 
-# wzór: xn+1 = xn-1 - [(f(xn-1) * (x0 - xn-1))/f(x0) - f(xn-1)]
-# wzór: xn = a - [(f(a) * (b - a ))/(f(b) - f(a))]
-def obliczenia():
-    # Zmienna wykorzystywana do sprawdzenia po którym razie program się zakończy
-    n = 1
-
-    x = sp.Symbol('x')
-
-    # Definiujemy epsilon
-    epsilon = float(input("Podaj epislon (ε):"))
-
-    # Definiujemy granice przedziału [a,b]
-    a = int(input("Podaj początek przedziału (a): "))
-    b = int(input("Podaj koniec przedziału (b): "))
-
-    # Sprawdzamy czy spełnione jest twierdzenie Bolzano-Cauchy'ego
-    if (twBolzanoCauchyego(a, b)):
-        if(sprawdzenie(a,b)):
-
-            while (True):
-                xn = round(a - ((f(a) * (b - a) )/ (f(b) - f(a))),3)
-                # print("Wynik dla x" + str(n) + " = " + str(xn))
-                # print(abs(f(xn)))
-                if(abs(f(xn)) < epsilon):
-                    print("Wynik dla x" + str(n) + " = " + str(xn))
-                    return abs(f(xn))
-                else:
-                    a = xn
-                    n = n + 1
-
-        else:
-            print("Sprawdzenie dla obydu granic przedziału jest fałszywe")
-    else:
-        print("Twierdzenie Bolzano-Cauchy'ego nie jest spełnione")
-
-
-# Wypisujemy wynik programu
-print(obliczenia())
-
+# Wyświetlamy wyniki
+print(f"Przybliżone miejsce zerowe funkcji f(x) = x ** 3 - 3 * x + 1 na przedziale [{a}, {b}] wynosi: {x0}")
